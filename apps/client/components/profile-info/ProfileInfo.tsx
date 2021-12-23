ec904befe40f3ff9e3ba2238c7b74430ae052e68
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-23 21:19:26 +0100

    Add stories

--- apps/client/components/profile-info/ProfileInfo.tsx
+++ apps/client/components/profile-info/ProfileInfo.tsx
@@ -15,13 +15,24 @@ export default function ProfileInfo() {
         <button className={`button-link ${styles.action}`}>Follow</button>
       </div>
       <div className={styles.info}>
-        <h1 className={styles.name}>Jane Doe</h1>
-        <p className={styles.bio}>
+        <h1>Jane Doe</h1>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, alias.
           Ipsum, alias. Tenetur, animi earum. Amet accusantium odio excepturi at
           minima ut, architecto, magnam pariatur culpa quaerat harum dolorem
           recusandae!
         </p>
+      </div>
+      <div className={styles.stories}>
+        {[1, 2, 3].map((i) => (
+          <div key={i}>
+            <img
+              src='https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces'
+              alt='Avatar'
+              className={styles.story}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
