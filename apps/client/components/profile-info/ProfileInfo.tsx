0f46f2ed88b2692d254d27f25508d470237d5d63
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-23 20:55:19 +0100

    Style username and follow

--- apps/client/components/profile-info/ProfileInfo.tsx
+++ apps/client/components/profile-info/ProfileInfo.tsx
@@ -3,11 +3,13 @@ import styles from './ProfileInfo.module.scss';
 export default function ProfileInfo() {
   return (
     <div className={styles.profileInfo}>
-      <img
-        src='https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces'
-        alt='Avatar'
-        className={styles.avatar}
-      />
+      <div className={styles.avatarContainer}>
+        <img
+          src='https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces'
+          alt='Avatar'
+          className={styles.avatar}
+        />
+      </div>
       <div className={styles.actions}>
         <h2 className={styles.username}>janedoe</h2>
         <button className={`button-link ${styles.action}`}>Follow</button>
