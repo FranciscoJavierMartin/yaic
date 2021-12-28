490c796ded97a3d90309c68415e0d393374e41b1
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-28 22:17:12 +0100

    second design

--- apps/client/pages/signin.tsx
+++ apps/client/pages/signin.tsx
@@ -17,23 +17,28 @@ export default function SignIn() {
 
   return (
     <div className={styles.signInPage}>
-      <h1>Yaic!</h1>
-      <form onSubmit={formik.handleSubmit} className={styles.signInForm}>
-        <input
-          name='email'
-          type='email'
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          placeholder='Email'
-        />
-        <input
-          name='password'
-          type='password'
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          placeholder='Password'
-        />
-        <button type='submit' className='button-link'>Sign In</button>
+      <div className={styles.background}>
+        <div className={styles.shape}></div>
+        <div className={styles.shape}></div>
+      </div>
+      <form>
+        <h3>Login Here</h3>
+
+        <label htmlFor='username'>Username</label>
+        <input type='text' placeholder='Email or Phone' id='username' />
+
+        <label htmlFor='password'>Password</label>
+        <input type='password' placeholder='Password' id='password' />
+
+        <button>Log In</button>
+        <div className={styles.social}>
+          <div className='go'>
+            <i className='fab fa-google'></i> Google
+          </div>
+          <div className='fb'>
+            <i className='fab fa-facebook'></i> Facebook
+          </div>
+        </div>
       </form>
     </div>
   );
