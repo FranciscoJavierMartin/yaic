4e95d52d8e3481faf7355423aa437297213a85a3
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2021-12-28 21:31:02 +0100

    first markup

--- apps/client/pages/signin.tsx
+++ apps/client/pages/signin.tsx
@@ -1,3 +1,40 @@
+import { useFormik } from 'formik';
+
+import styles from './signin.module.scss';
+
+interface SignInValues {
+  password: string;
+  email: string;
+}
+
 export default function SignIn() {
-  return <div>Sign In</div>;
+  const formik = useFormik<SignInValues>({
+    initialValues: { email: '', password: '' },
+    onSubmit: (values: SignInValues) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <div className={styles.signInPage}>
+      <h1>Yaic!</h1>
+      <form onSubmit={formik.handleSubmit} className={styles.signInForm}>
+        <input
+          name='email'
+          type='email'
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          placeholder='Email'
+        />
+        <input
+          name='password'
+          type='password'
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          placeholder='Password'
+        />
+        <button type='submit' className='button-link'>Sign In</button>
+      </form>
+    </div>
+  );
 }
